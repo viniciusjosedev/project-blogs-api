d0866044dd6de85c4edad0472a0c0e633079ce5c
Vinicius José <viniciusjosedev@gmail.com>
2023-05-12 14:02:46 -0300

    feat: requirement 2 done

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+	const userModel = sequelize.define('User', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false
+		},
+		displayName: {
+			field: 'display_name',
+			allowNull: false,
+			type: DataTypes.STRING,
+		},
+		email: {
+			allowNull: false,
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		password: {
+			allowNull: false,
+			type: DataTypes.STRING,
+		},
+		image: {
+			allowNull: true,
+			type: DataTypes.STRING,
+		}
+	},
+	{
+		tableName: 'users',
+		underscored: true,
+		timestamps: false
+	});
+
+  return userModel;
+}
