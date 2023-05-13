b24586c8f874992f1c811d0f0c6dbc54806ba174
Vinicius José <viniciusjosedev@gmail.com>
2023-05-12 21:08:34 -0300

    feat: requirement 11 done

---
+++ src/models/PostCategory.js
@@ -0,0 +1,47 @@
+module.exports = (Sequelize, DataTypes) => {
+  const postCategorySchema = Sequelize.define('PostCategory', {
+		postId: {
+			primaryKey: true,
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'blog_posts',
+				key: 'id'
+			},
+			field: 'post_id',
+			onUpdate: 'CASCADE',
+			onDelete: 'CASCADE'
+		},
+		categoryId: {
+			primaryKey: true,
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'categories',
+				key: 'id'
+			},
+			field: 'category_id',
+			onUpdate: 'CASCADE',
+			onDelete: 'CASCADE'
+		}
+	}, {
+		tableName: 'PostCategory',
+		underscored: true,
+		timestamps: false,
+	});
+
+	postCategorySchema.associate = (models) => {
+		models.BlogPost.belongsToMany(models.Category, {
+			through: postCategorySchema,
+			foreignKey: 'postId',
+			otherKey: 'categoryId',
+			as: 'BlogPost'
+		});
+		models.Category.belongsToMany(models.BlogPost, {
+			through: postCategorySchema,
+			foreignKey: 'categoryId',
+			otherKey: 'postId',
+			as: 'Category'
+		})
+	};
+
+	return postCategorySchema;
+}
